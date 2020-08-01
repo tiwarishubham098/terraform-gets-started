83c57bd79d07bc3a4f0849bdd2c708b2f02d477c
Rakesh Suryawanshi <67486994+krishrocks1904@users.noreply.github.com>
2020-08-01 21:50:16 +0100

    Update provider.tf

--- Iac/provider.tf
+++ Iac/provider.tf
@@ -2,10 +2,10 @@
 provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "~>2.20.0"
-   SUBSCRIPTION_ID: "2a04288a-8136-4880-b526-c6070e59f004"
-   TENANT_ID: "37d20c78-05e3-416d-83ab-cdbc21fed22a"
-   CLIENT_ID: "ecc4be67-51ce-4780-a17a-75426c4b8a7a"
-   CLIENT_SECRET : var.CLIENT_SECRET
+  subscription_id = "2a04288a-8136-4880-b526-c6070e59f004"
+  tenant_id = "37d20c78-05e3-416d-83ab-cdbc21fed22a"
+  client_id = "ecc4be67-51ce-4780-a17a-75426c4b8a7a"
+  client_secret  = var.CLIENT_SECRET
   features {}
 }
 
