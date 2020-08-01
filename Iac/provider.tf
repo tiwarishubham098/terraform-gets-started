da4792777017132464e0f868879e465c73ded39f
Rakesh Suryawanshi <67486994+krishrocks1904@users.noreply.github.com>
2020-08-01 22:26:59 +0100

    Update provider.tf

--- Iac/provider.tf
+++ Iac/provider.tf
@@ -1,6 +1,6 @@
 # Configure the Azure Provider
 provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  # whilst the `xversion` attribute is optional, we recommend pinning to a given version of the Provider
   version = "~>2.20.0"
   subscription_id = "2a04288a-8136-4880-b526-c6070e59f004"
   tenant_id = "37d20c78-05e3-416d-83ab-cdbc21fed22a"
