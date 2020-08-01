73e94501b75b97631b5403b07d1343a8fb68bdf6
Rakesh Suryawanshi <67486994+krishrocks1904@users.noreply.github.com>
2020-08-01 20:48:30 +0100

    Update provider.tf

--- Iac/provider.tf
+++ Iac/provider.tf
@@ -5,13 +5,13 @@ provider "azurerm" {
   features {}
 }
 
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "BEE-A-LEARNER"
-    token = "EsV6Il3gXoLGag.atlasv1.rVYclUQzBGY4NYndPECz21o2J3AOjSfjBJd6BmObnAsaEbfw3Ad0RDykzhMK9FSYpyI"
-    workspaces {
-      name = "LER-TST"
-    }
-  }
-}
+# terraform {
+#   backend "remote" {
+#     hostname = "app.terraform.io"
+#     organization = "BEE-A-LEARNER"
+#     token = "EsV6Il3gXoLGag.atlasv1.rVYclUQzBGY4NYndPECz21o2J3AOjSfjBJd6BmObnAsaEbfw3Ad0RDykzhMK9FSYpyI"
+#     workspaces {
+#       name = "LER-TST"
+#     }
+#   }
+# }
