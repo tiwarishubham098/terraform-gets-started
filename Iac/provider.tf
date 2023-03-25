d04142e97b66b3ce5004c5f11920e52707ec227e
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-03-25 13:15:27 +0530

    Update provider.tf

--- Iac/provider.tf
+++ Iac/provider.tf
@@ -1,10 +1,10 @@
 # Configure the Azure Provider
 provider "azurerm" {
   # whilst the `xversion` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "~>2.20.0"
-  subscription_id = "2a04288a-8136-4880-b526-c6070e59f004"
-  tenant_id = "37d20c78-05e3-416d-83ab-cdbc21fed22a"
-  client_id = "ecc4be67-51ce-4780-a17a-75426c4b8a7a"
+  version = "~>3.20.0"
+  subscription_id = "85cd2292-82e3-4c72-a2d7-1ba724a25176"
+  tenant_id = "120709b9-1cde-4a68-944a-f6fb5b566111"
+  client_id = "80519e20-919b-4d81-a8d9-405d697d2644"
   client_secret  = var.client_secret
   features {}
 }
