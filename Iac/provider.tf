0a45ae344237cce1ba7025c8f170f713f0253db1
tiwarishubham098 <112696751+tiwarishubham098@users.noreply.github.com>
2023-03-30 17:04:30 +0530

    Update provider.tf

--- Iac/provider.tf
+++ Iac/provider.tf
@@ -1,9 +1,9 @@
 # Configure the Azure Provider
 provider "azurerm" {
-  subscription_id = "85cd2292-82e3-4c72-a2d7-1ba724a25176"
-  tenant_id = "120709b9-1cde-4a68-944a-f6fb5b566111"
-  client_id = "80519e20-919b-4d81-a8d9-405d697d2644"
-  client_secret  = var.client_secret
+ subscription_id = "85cd2292-82e3-4c72-a2d7-1ba724a25176"
+ tenant_id ="120709b9-1cde-4a68-944a-f6fb5b566111"
+ client_id ="cfdd0e4a-063c-483b-914a-462ffc43a451"
+ client_secret ="YNY8Q~6tgGMJ_tvheVvFyXG78SsUbL66-mS.rb45
   features {}
 }
 
